18bdecbacd21d166a8a8ea7a70e95b9b23bbdaa6
Mikkim <gawrmi2015@cranleigh.org>
2016-11-07 09:47:52 +0000

    Add files via upload

---
+++ light crossing mikolaj.py
@@ -0,0 +1,5 @@
+lightcolour = input ("is the light green Y/N")
+if lightcolour == "Y" :
+    print("please cross safely")
+else:
+    print("wait until the green light plaese")
---
+++ password mikolaj.py
@@ -0,0 +1,6 @@
+password = input("please enter your password")
+passwordinputted = input("please enter your password again please")
+if password == passwordinputted :
+    print("access granted, thank you for using our website")
+else:
+    print("sorry you entered the wrong password, try again later")
